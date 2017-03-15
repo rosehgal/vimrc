a52591298e56f7863f55cd1b2e1e26bc3bfad73a
Rohit Sehgal <rsehgal@iitk.ac.in>
2017-03-15 19:39:37 +0530

    Auto save

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ls -l ~/.vimrc
 if [ $? -ne 0 ];then touch ~/.vimrc;fi
 curl https://raw.githubusercontent.com/r0hi7/vimrc/master/vimrc > ~/.vimrc
-vim -c ":PluginInstall"
+vim -c ":PluginInstall" -c "q!" -c "q!" 
