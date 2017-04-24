9043d9382880f2cda1f9b972cb13f7ac69f44c50
Rohit Sehgal <rsehgal@iitk.ac.in>
2017-04-25 00:32:34 +0530

    Script updated

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ls -l ~/.vimrc
 if [ $? -ne 0 ];then touch ~/.vimrc;fi
 curl https://raw.githubusercontent.com/r0hi7/vimrc/master/vimrc > ~/.vimrc
 vim +PluginInstall +qall;
+mkdir -p ~/.vim/colors
 curl https://raw.githubusercontent.com/r0hi7/vimrc/master/pencil.vim > ~/.vim/colors/pencil.vim
 curl https://raw.githubusercontent.com/r0hi7/vimrc/master/airline-pencil.vim > ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/pencil.vim
 
